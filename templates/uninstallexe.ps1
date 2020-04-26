98c2be27813837a9b70e9fe72d25c774fb691a98
Adrian Cooper <adriancooper81@hotmail.com>
2020-04-26 13:53:48 +0930

    Update uninstallexe.ps1

--- templates/uninstallexe.ps1
+++ templates/uninstallexe.ps1
@@ -4,5 +4,5 @@ $processname = "process.exe"
 if(Get-Package -Provider Programs -IncludeWindowsInstaller -Name "$programname" -ErrorAction SilentlyContinue)
 {
    taskkill /F /IM $processname /FI 'status eq running'
-   &\"$ProgramFilesPath\program\uninstall.exe\" /S | Out-Null
+   &"$ProgramFilesPath\program\uninstall.exe" /S | Out-Null
 }
