e828aa76ad847bfe5f5f6a62caa8b2c292a549c1
Adrian Cooper <adriancooper81@hotmail.com>
2021-04-15 22:38:48 +0930

    Update updates.ps1

--- bin/updates.ps1
+++ bin/updates.ps1
@@ -2,7 +2,10 @@
 ForEach ($line in $($status -split "`r`n"))
 {
     $line = $line.TrimStart()
+    Write-Host $line
     $line = $line.Split(":")
+    Write-Host $line
     $line = $line[0].TrimEnd()
-    iex scoop update $line -g
+    Write-Host $line
+    iex "scoop update $line -g"
 }
