82bc29f1aebe02de0c614364c999cbba08f5c317
Adrian Cooper <adriancooper81@hotmail.com>
2021-04-15 15:30:30 +0930

    Create updates.ps1

---
+++ bin/updates.ps1
@@ -0,0 +1,8 @@
+﻿scoop update; $status = scoop status
+ForEach ($line in $($status -split "`r`n"))
+{
+    $line = $line.TrimStart()
+    $line = $line.Split(":")
+    $line = $line[0].TrimEnd()
+    iex scoop update $line -g
+}
