6b57e9e3210e6eb200bbe9342f4e3e9549e92fc7
Adrian Cooper <adriancooper81@hotmail.com>
2021-04-15 23:50:02 +0930

    Update scoopupdatesalias.ps1

--- bin/scoopupdatesalias.ps1
+++ bin/scoopupdatesalias.ps1
@@ -1,2 +1,2 @@
-﻿scoop alias add "Updates" 'scoop update; $status = scoop status; ForEach ($line in $($status -split "`r`n")) { $line = $line.TrimStart().Split(":")[0].TrimEnd(); Invoke-Expression -Command "scoop update $line"; Invoke-Expression -Command "sudo scoop update $line -g" }; Start-Sleep -Seconds 10' "Updates all apps one at a time"
+﻿scoop alias add "Updates" 'scoop update; $status = scoop status; ForEach ($line in $($status -split "`r`n")) { $line = $line.TrimStart().Split(":")[0].TrimEnd(); Invoke-Expression -Command "scoop update $line -k"; Invoke-Expression -Command "sudo scoop update $line -g -k" }; Start-Sleep -Seconds 10' "Updates all apps one at a time"
 Start-Sleep -Seconds 15
