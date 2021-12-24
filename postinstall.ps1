48c4f946caeea2562a8b3f58a49410a85c8b976e
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 18:09:34 +1030

    Update postinstall.ps1

--- postinstall.ps1
+++ postinstall.ps1
@@ -3,3 +3,4 @@ sudo scoop list
 Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
 scoop update
+scoop checkup
