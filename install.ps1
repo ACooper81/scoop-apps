152176393cf18b208f31964c87cdc69229a0fa17
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 17:52:43 +1030

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -6,10 +6,3 @@ scoop bucket add 'Base'
 scoop bucket add Scoop-Apps 'https://github.com/ACooper81/scoop-apps'
 [Environment]::SetEnvironmentVariable('SCOOP', '$env:UserProfile\scoop', 'User')
 scoop update
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs }
-scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
-sudo scoop list
-Add-MpPreference -ExclusionPath 'C:\Users\User\scoop'
-Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
-Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
-scoop update
