1879aed30767c381885aeb7fb23c2e28b7dce574
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 17:38:18 +1030

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,15 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' | iex
 scoop install sudo git
-sudo scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
 scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core'
+scoop bucket add 'Base'
 scoop bucket add Scoop-Apps 'https://github.com/ACooper81/scoop-apps'
+[Environment]::SetEnvironmentVariable('SCOOP', '$env:UserProfile\scoop', 'User')
 scoop update
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
 sudo scoop list
-scoop checkup
+Add-MpPreference -ExclusionPath 'C:\Users\User\scoop'
+Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+scoop update
