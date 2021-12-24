eab2607d8e3d138868f6651baaa2064dc1f6dbad
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 17:56:06 +1030

    Update postinstall.ps1

--- postinstall.ps1
+++ postinstall.ps1
@@ -1,4 +1,3 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs }
 scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
 sudo scoop list
 Add-MpPreference -ExclusionPath 'C:\Users\User\scoop'
