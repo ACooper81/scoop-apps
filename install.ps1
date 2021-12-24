220f2b0a4b6337cd8bb2a607f06e33a8fc8c1800
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 17:12:41 +1030

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,9 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' | iex
-scoop install sudo
+scoop install sudo git
 sudo scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
 scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core'
 scoop bucket add Scoop-Apps 'https://github.com/ACooper81/scoop-apps'
 scoop update
+sudo scoop list
 scoop checkup
