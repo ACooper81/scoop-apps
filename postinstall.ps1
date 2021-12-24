903823ffc592ac5225ab268d60de9cfcb30bb373
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 18:16:59 +1030

    Update postinstall.ps1

--- postinstall.ps1
+++ postinstall.ps1
@@ -1,4 +1,4 @@
-scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
+scoop install aria2 7zip git sudo dark innounp lessmsi scoop-search --global --no-cache
 sudo scoop list
 Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
