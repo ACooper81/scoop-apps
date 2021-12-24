c7c293672245e0fb59bf371d24a0469bd89b042d
Adrian Cooper <adriancooper81@hotmail.com>
2021-12-24 17:53:42 +1030

    Update postinstall.ps1

--- postinstall.ps1
+++ postinstall.ps1
@@ -1,1 +1,7 @@
-
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs }
+scoop install 7zip git sudo dark innounp lessmsi aria2 --global --no-cache
+sudo scoop list
+Add-MpPreference -ExclusionPath 'C:\Users\User\scoop'
+Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+scoop update
