3b085f56b1253a14115db77e2c81d17501d1c0a3
Adrian Cooper <adriancooper81@hotmail.com>
2022-06-04 20:07:49 +0930

    Update InstallAllPortableApps.ps1

--- scripts/InstallAllPortableApps.ps1
+++ scripts/InstallAllPortableApps.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -Path "$env:userprofile\scoop\buckets\scoop-apps\bucket" -File -Filter "*Portable.json" | Select BaseName | foreach {scoop update $_.BaseName -g}
+Get-ChildItem -Path "$env:userprofile\scoop\buckets\scoop-apps\bucket" -File -Filter "*Portable.json" | Select BaseName | foreach {scoop install $_.BaseName}
