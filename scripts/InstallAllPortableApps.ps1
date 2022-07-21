4f48b7977088320d163e7729dadfe26377bc1931
Adrian Cooper <adriancooper81@hotmail.com>
2022-07-21 21:48:50 +0930

    Update InstallAllPortableApps.ps1

--- scripts/InstallAllPortableApps.ps1
+++ scripts/InstallAllPortableApps.ps1
@@ -1,2 +1,2 @@
-$Excluded = "*Nightly-*","*NightlyCLI-*","*Alpha-*","*AlphaCLI-*","*Beta-*","*BetaCLI-*","*RC-*","*RCCLI-*","*Nightly-*","*NightlyCLI-*","*Nightly-*","*NightlyCLI-*"
+$Excluded = "*Nightly-*","*NightlyCLI-*","*Alpha-*","*AlphaCLI-*","*Beta-*","*BetaCLI-*","*RC-*","*RCCLI-*","*Dev-*","*DevCLI-*","*Nightly-*","*NightlyCLI-*"
 Get-ChildItem -Path "$env:userprofile\scoop\buckets\scoop-apps\bucket" -File -Recurse -Filter "*Portable.json" -Exclude $Excluded | Select-Object BaseName | ForEach-Object { If(!(test-path -PathType container ("$env:userprofile\scoop\apps\" + $_.Basename))) {Write-Host $_.BaseName}}
