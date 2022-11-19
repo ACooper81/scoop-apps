3896b3adefd92cc7ada19b4cf64f28659a65ae66
Adrian Cooper <adriancooper81@hotmail.com>
2022-11-19 14:19:52 +1030

    Update installAsAdmin.ps1

--- scripts/Scoop/installAsAdmin.ps1
+++ scripts/Scoop/installAsAdmin.ps1
@@ -1,13 +1,11 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod get.scoop.sh -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin -ScoopDir "$env:USERPROFILE\scoop" -ScoopGlobalDir "$env:ProgramData\scoop" -ScoopCacheDir "$env:USERPROFILE\scoop\cache" -NoProxy
-scoop install gsudo git scoop-search
-# scoop config SCOOP_REPO 'https://github.com/Ash258/Scoop-Core'
-scoop update
+scoop install gsudo git scoop-search aria2 7zip dark innounp lessmsi
 # scoop bucket add 'Base'
 scoop bucket add Scoop-Apps 'https://github.com/ACooper81/scoop-apps'
-# gsudo
-scoop install aria2 7zip git gsudo dark innounp lessmsi scoop-search --global --no-cache
+scoop update
+# scoop install aria2 7zip git gsudo dark innounp lessmsi scoop-search --global --no-cache
 Add-MpPreference -ExclusionPath "$env:ProgramData\scoop"
 Add-MpPreference -ExclusionPath 'C:\ProgramData\scoop'
 [Environment]::SetEnvironmentVariable('SCOOP', "$env:USERPROFILE\scoop", 'User')
